22357f02691d9011ab1ed0d25436f86e7a1b8ec1
Nareze <nabrez78@gmail.com>
2021-09-16 20:03:26 +0200

    amelioration theme

--- index.js
+++ index.js
@@ -3,101 +3,50 @@ function showTeddies(){
       .then(function(res) {
         return res.json();
     })
-      .then (function(value){
-        console.log(value);
-        for (let i = 0; i < 5; i++){
-          let list = document.createElement("LI");
+      .then (function(teddies){
+        console.log(teddies);
+        for (let teddy in teddies){
+          let list = document.createElement("a"); /* On créee l'élement parent qui englobe l'image et la description */
+          list.setAttribute('href','/produit.html');
+          list.classList.add('productPageCenter');
+
+
           let image = document.createElement("img");
-          image.setAttribute('src',value[i].imageUrl);
-          image.style.width = '40%';
-          image.style.padding = '20px';
-          image.style.borderRadius = '55px';
-          list.appendChild(image);
-          document.getElementById("teddies").appendChild(list);
+          image.setAttribute('src',teddies[teddy].imageUrl); /* On donne les propriétés à l'image */
+          image.style.borderRadius = '50px';
+          image.style.padding = '25px';
+          image.setAttribute("width", "304");
+          image.setAttribute("height", "228");
+          list.appendChild(image); /* On ajoute l'élément image dans l'élément parent lien */
+
+
+
+
+          
           let name = document.createElement("name");
-          name.innerHTML = value[i].name + ' :';
-          name.style.marginRight = '10px';
-          name.style.paddingLeft = '10px';
+          name.innerHTML = teddies[teddy].name + ' :';
+          name.classList.add('marginDesc');
+          name.style.marginRight = '';
+          name.style.paddingLeft = '5px';
+          name.style.fontSize = '20px';
           name.style.borderLeft = '5px solid #AF6BA5';
           name.style.color = 'black';
           name.style.fontWeight = 'bold';
           list.appendChild(name);
           let price = document.createElement("price");
-          price.innerHTML = (value[i].price)/100 + '€';
+          price.innerHTML = (teddies[teddy].price)/100 + '€';
+          price.classList.add('marginDesc');
+          price.style.fontSize = '18px';
           price.style.fontWeight = "bold";
           price.style.fontStyle = "italic";
           list.appendChild(price);
+
+
+
+
+          document.getElementById("teddies").appendChild(list); /* On inscrit l'élement parent dans la div voulue */
+          /* ajouter catch */
       }
     })
     }
     showTeddies();
-    
-    
-    function showCamera(){
-      fetch("http://localhost:3000/api/cameras")
-        .then(function(res) {
-          return res.json();
-      })
-        .then (function(value){
-          console.log(value);
-            for (let i = 0; i < 5; i++){
-              let list = document.createElement("LI");
-              let image = document.createElement("img");
-              image.setAttribute('src',value[i].imageUrl);
-              image.style.width = '40%';
-              image.style.padding = '20px';
-              image.style.borderRadius = '55px';
-              list.appendChild(image);
-              document.getElementById("camera").appendChild(list);
-              let name = document.createElement("name");
-              name.innerHTML = value[i].name + ' :';
-              name.style.marginRight = '10px';
-              name.style.paddingLeft = '10px';
-              name.style.borderLeft = '5px solid #AF6BA5';
-              name.style.color = 'black';
-              name.style.fontWeight = 'bold';
-              list.appendChild(name);
-              let price = document.createElement("price");
-              price.innerHTML = (value[i].price)/100 + '€';
-              price.style.fontWeight = "bold";
-              price.style.fontStyle = "italic";
-              list.appendChild(price);
-          }
-      })
-      }
-      showCamera();
-    
-    
-      function showFurniture(){
-        fetch("http://localhost:3000/api/furniture")
-          .then(function(res) {
-            return res.json();
-        })
-          .then (function(value){
-            console.log(value);
-              for (let i = 0; i < 5; i++){
-                let list = document.createElement("LI");
-                let image = document.createElement("img");
-                image.setAttribute('src',value[i].imageUrl);
-                image.style.width = '40%';
-                image.style.padding = '20px';
-                image.style.borderRadius = '55px';
-                list.appendChild(image);
-                document.getElementById("furniture").appendChild(list);
-                let name = document.createElement("name");
-                name.innerHTML = value[i].name + ' :';
-                name.style.marginRight = '10px';
-                name.style.paddingLeft = '10px';
-                name.style.borderLeft = '5px solid #AF6BA5';
-                name.style.color = 'black';
-                name.style.fontWeight = 'bold';
-                list.appendChild(name);
-                let price = document.createElement("price");
-                price.innerHTML = (value[i].price)/100 + '€';
-                price.style.fontWeight = "bold";
-                price.style.fontStyle = "italic";
-                list.appendChild(price);
-            }
-        })
-        }
-        showFurniture();
