993b95eeed8b47853f643d4c40c1d3b9186c0320
Nareze <nabrez78@gmail.com>
2021-09-18 19:48:49 +0200

    ajout personnalisation produit et formulaire page panier

--- index.js
+++ index.js
@@ -1,44 +1,70 @@
-function showTeddies(){
-    fetch("http://localhost:3000/api/teddies")
-      .then(function(res) {
-        return res.json();
+function showTeddies() {
+  fetch("http://localhost:3000/api/teddies")
+    .then(function (res) {
+      return res.json();
     })
-      .then (function(teddies){
-        console.log(teddies);
-        for (let teddy in teddies){
-          let list = document.createElement("a"); /* On créee l'élement parent qui englobe l'image et la description */
-          list.setAttribute('href', "produit.html?" + teddies[teddy]._id );   /* On ajoute l'id correspondant au produit dans le lien avec le point d'interrogation */
-          list.classList.add('productPageCenter');
+    .then(function (teddies) {
+      console.log(teddies);
+      for (let teddy in teddies) {
+        let list =
+          document.createElement(
+            "a"
+          ); /* On créee l'élement parent qui englobe l'image et la description */
+        list.setAttribute(
+          "href",
+          "produit.html?" + teddies[teddy]._id
+        ); /* On ajoute l'id correspondant au produit dans le lien avec le point d'interrogation */
 
+        /*Inscription des liens dans le footer */
+        let footerLink_1 = document.getElementById("test2");
+        footerLink_1.innerHTML = teddies[2].name;
+        footerLink_1.setAttribute("href", "produit.html?" + teddies[2]._id);
+        let footerLink_2 = document.getElementById("test3");
+        footerLink_2.innerHTML = teddies[4].name;
+        footerLink_2.setAttribute("href", "produit.html?" + teddies[4]._id);
+        let footerLink_3 = document.getElementById("test4");
+        footerLink_3.innerHTML = teddies[3].name;
+        footerLink_3.setAttribute("href", "produit.html?" + teddies[3]._id);
 
-          let image = document.createElement("img");
-          image.setAttribute('src',teddies[teddy].imageUrl); /* On donne les propriétés à l'image */
-          image.style.borderRadius = '50px';
-          image.style.paddingBottom = '20px';
-          image.setAttribute("width", "304");
-          image.setAttribute("height", "228");
-          list.appendChild(image); /* On ajoute l'élément image dans l'élément parent lien */
+        let image = document.createElement("img");
+        image.setAttribute(
+          "src",
+          teddies[teddy].imageUrl
+        ); /* On donne les propriétés à l'image */
+        image.style.borderRadius = "50px";
+        image.style.paddingBottom = "20px";
+        image.setAttribute("width", "304");
+        image.setAttribute("height", "228");
+        list.appendChild(
+          image
+        ); /* On ajoute l'élément image dans l'élément parent lien */
 
+        let name = document.createElement("p");
+        name.innerHTML = teddies[teddy].name + " :";
+        name.style.marginRight = "";
+        name.style.paddingLeft = "5px";
+        name.style.fontSize = "20px";
+        name.style.fontWeight = "bold";
+        list.appendChild(name);
 
-          let name = document.createElement("p");
-          name.innerHTML = teddies[teddy].name + ' :';
-          name.style.marginRight = '';
-          name.style.paddingLeft = '5px';
-          name.style.fontSize = '20px';
-          name.style.fontWeight = 'bold';
-          list.appendChild(name);
-
-
-          let price = document.createElement("span");
-          price.innerHTML = (teddies[teddy].price)/100 + '€';
-          price.classList.add('marginDesc');
-          price.style.fontSize = '18px';
-          price.style.fontWeight = "bold";
-          price.style.fontStyle = "italic";
-          name.appendChild(price);  /* On ajoute le prix dans le paragraphe, sur la même ligne */
-          document.getElementById("teddies").appendChild(list); /* On inscrit l'élement parent dans la div voulue */
-          /* ajouter catch */
+        let price = document.createElement("span");
+        price.innerHTML = teddies[teddy].price / 100 + "€";
+        price.style.fontSize = "18px";
+        price.style.fontWeight = "bold";
+        price.style.fontStyle = "italic";
+        name.appendChild(
+          price
+        ); /* On ajoute le prix dans le paragraphe, sur la même ligne */
+        document
+          .getElementById("teddies")
+          .appendChild(
+            list
+          ); /* On inscrit l'élement parent dans la div voulue */
+        /* ajouter catch */
       }
     })
-    }
-    showTeddies();
+    .catch(function (teddies) {
+      console.log("Il y a eu un problème avec l'opération fetch");
+    });
+}
+showTeddies();
--- produit.js
+++ produit.js
@@ -1,37 +1,80 @@
-let url_id = window.location.search.substr(1);  /* On enlève le '?' pour créer l'id correspondant au produit */
-console.log(url_id); 
+let url_id =
+  window.location.search.substr(
+    1
+  ); /* On enlève le '?' pour créer l'id correspondant au produit */
+console.log(url_id);
 
-
-function showTeddies(){
-    fetch(`http://localhost:3000/api/teddies/${url_id}`)        /* Affichage du produit correspondant grâce à l'id ajouté dans l'adresse */
-      .then(function(res) {
-        return res.json();
+function showTeddies() {
+  fetch(
+    `http://localhost:3000/api/teddies/${url_id}`
+  ) /* Affichage du produit correspondant grâce à l'id ajouté dans l'adresse */
+    .then(function (res) {
+      return res.json();
     })
-      .then (function(teddies){
-          console.log(teddies);
-          list = document.getElementById('teddies');
-          let image = document.createElement("img");
-          image.setAttribute('src',teddies.imageUrl);
-          image.setAttribute("width", "304");
-          image.setAttribute("height", "228");
-          image.style.borderRadius = '50px';
-          list.appendChild(image);
-          let name = document.createElement("p");
-          name.style.marginTop = '25px';
-          name.style.fontSize = '20px';
-          name.style.fontWeight = 'bold';
-          name.innerHTML = teddies.name + ` :`;
-          list.appendChild(name)
-          let price = document.createElement("span");
-          price.style.marginLeft = '10px';
-          price.innerHTML = (teddies.price)/100 + `€`;
-          name.appendChild(price);
-          let desc = document.createElement("p");
-          desc.style.fontSize = '15px';
-          desc.style.fontWeight = 'bold';
-          desc.innerHTML = teddies.description;
-          list.appendChild(desc);
-
+    .then(function (teddies) {
+      console.log(teddies);
+      list = document.getElementById("teddies");
+      let image = document.createElement("img");
+      image.setAttribute("src", teddies.imageUrl);
+      image.setAttribute("width", "304");
+      image.setAttribute("height", "228");
+      image.style.borderRadius = "50px";
+      list.appendChild(image);
+      let name = document.createElement("p");
+      name.style.marginTop = "25px";
+      name.style.fontSize = "20px";
+      name.style.fontWeight = "bold";
+      name.innerHTML = teddies.name + ` :`;
+      list.appendChild(name);
+      let price = document.createElement("span");
+      price.style.marginLeft = "10px";
+      price.innerHTML = teddies.price / 100 + `€`;
+      name.appendChild(price);
+      let desc = document.createElement("p");
+      desc.style.fontSize = "15px";
+      desc.style.fontWeight = "bold";
+      desc.innerHTML = teddies.description;
+      list.appendChild(desc);
     })
-    }
-    showTeddies();
+    .catch(function (teddies) {
+      console.log("Il y a eu un problème avec l'opération fetch");
+    });
+}
+showTeddies();
+
+function showCustomColors() {
+  fetch(
+    `http://localhost:3000/api/teddies/${url_id}`
+  ) /* Affichage du produit correspondant grâce à l'id ajouté dans l'adresse */
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (custom) {
+      console.log(custom);
+      for (let color of custom.colors) {
+        /* Utilisation de la boucle for of pour iterer sur les valeurs */
+        console.log(color);
+        let customColor = document.createElement("option");
+        let list = document.getElementById("customColors");
+        customColor.innerText = color;
+        list.appendChild(customColor);
+      }
+    })
+    .catch(function (teddies) {
+      console.log("Il y a eu un problème avec l'opération fetch");
+    });
+}
+showCustomColors();
+
+let totalProduct = document.getElementById("numberItemCard");
+let addProduct = document.getElementById("addToCard");
+let removeProduct = document.getElementById("deleteFromCard");
+let product = 0;
+
+addProduct.addEventListener("click", (event) => {
+  totalProduct.innerText = product++;
+});
+
+removeProduct.addEventListener("click", (event) => {
+  totalProduct.innerText = product--;
+});
